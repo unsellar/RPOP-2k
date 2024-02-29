3a0ed0bcaaf13f1de2e083e1f1772dc34cd6b622
unsellar <blinov.ceva2015@yandex.ru>
2024-02-29 18:51:49 +0300

    task 2 made

--- RPOP/main.cpp
+++ RPOP/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-using namespace std
+#include <fstream>
+#include <regex>
+using namespace std;
 //(hel*o)*wrld
 
 
-int main(int argc, const char * argv[]) {
-    
+int main() {
+    string input;
+    ifstream file ("/Users/vsevolod/Documents/RPOP/RPOP/input.txt");
+    while (getline(file, input))    {
+        cout << "input line is '" << input << "  ";
+        if (regex_match(input, regex("(hel*o)* wrld")) == 1) cout << "answer is true" << endl;
+        else cout << "'  answer is false" << endl;
+    }
     return 0;
 }
