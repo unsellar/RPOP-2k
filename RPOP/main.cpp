82550a9991b0707a7a0894f0dd58cab5718b5e3a
unsellar <blinov.ceva2015@yandex.ru>
2024-02-29 18:17:06 +0300

    Update main.cpp

--- RPOP/main.cpp
+++ RPOP/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  RPOP
-//
-//  Created by Vsevolod Blinov on 29.02.2024.
-//
-
 #include <iostream>
+using namespace std
+//(hel*o)*wrld
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
