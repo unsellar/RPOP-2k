2c3f4be0526a8382d87ec26d675386910dbf6389
unsellar <blinov.ceva2015@yandex.ru>
2024-02-29 18:14:38 +0300

    Initial Commit

---
+++ RPOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RPOP
+//
+//  Created by Vsevolod Blinov on 29.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
