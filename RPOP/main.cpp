6bf5c8c98effbb1c8865c55457c808859511b652
unsellar <blinov.ceva2015@yandex.ru>
2024-03-01 12:40:09 +0300

    task done and tested

--- RPOP/main.cpp
+++ RPOP/main.cpp
@@ -2,16 +2,54 @@
 #include <fstream>
 #include <regex>
 using namespace std;
-//(hel*o)*wrld
+
+
+//(hel*o)* wrld regex
+
+
+string hello_checker(string input){
+    if(input[0] == 'h' and input[1] == 'e'){                     //first two HE
+        input.erase(0, 2);
+        
+        if(input[0] == 'l'){                                     // lll continuation
+            while(input[0] == 'l'){
+                input.erase(0, 1);
+            }
+        }
+        
+        if (input[0] == 'o'){                                    // o last erase
+            input.erase(0, 1);
+            return input;
+        }
+    } else {
+        return "e";
+    }
+    return "e";
+}
+
+bool wrld_checker(string input){
+    if(input.compare(" wrld") == 0) return true;
+    return false;
+}
 
 
 int main() {
     string input;
     ifstream file ("/Users/vsevolod/Documents/RPOP/RPOP/input.txt");
-    while (getline(file, input))    {
-        cout << "input line is '" << input << "  ";
-        if (regex_match(input, regex("(hel*o)* wrld")) == 1) cout << "answer is true" << endl;
-        else cout << "'  answer is false" << endl;
+    while(getline(file, input)){
+        cout << "input line is " << input;
+        if(wrld_checker(input)){
+            cout << "     answer is true!" << endl;
+        }else{
+            if(hello_checker(input) != "e"){//1st hello
+                input = hello_checker(input);
+                while(hello_checker(input) != "e"){//2+hello erasing
+                    input = hello_checker(input);
+                }
+                if(wrld_checker(input)) cout << "     answer is true" << endl;
+                else cout << "  answer is false" << endl;
+            }else cout << "  answer is false" << endl;
+        }
     }
     return 0;
 }
