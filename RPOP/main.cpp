e742b59e4075ee4fb92a85d9fd4f91896b673007
unsellar <blinov.ceva2015@yandex.ru>
2024-05-23 14:18:54 +0300

    simple error checker + remade task3

--- RPOP/main.cpp
+++ RPOP/main.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 
 //(hel*o)* wrld regex
+bool isNumber(const std::string& str) {
+    try {
+        int num = std::stoi(str);
+        return true;
+    } catch(std::invalid_argument& e) {
+        return false;
+    }
+}
 
 
 string hello_checker(string input){
@@ -66,7 +74,16 @@ bool lexer(){
         {"else", "T_else"},
         {"while", "T_while"},
         {"for", "T_for"},
-        {"return", "T_return"}
+        {"return", "T_return"},
+        {"printf", "T_printf"}
+    };
+    
+    map<string, string> indents_allowers = {
+        {"int", "T_int"},
+        {"char", "T_char"},
+        {"float", "T_float"},
+        {"void", "T_void"},
+        
     };
 
     map<string, string> symbols = {
@@ -95,39 +112,91 @@ bool lexer(){
         {",", "T_comma"}
     };
     
+    string line;
+    string delimeter = " ";
+    bool indentcreation = 0;             //возможность инициализации переменной
+    int indentcreation_expire = 0;
+    string indentslist[10];              //список существующих переменных
+    int numberofindents = 0;
+    bool functionbody = 0;
+    
+    string commentstopper = "\*/";
     string input;
     ifstream file ("/Users/vsevolod/Documents/RPOP/RPOP/input2.txt");
-    while(file >> input){
-        if(input == "/*"){                                     //comments
-            while(input != "*/"){
-                file >> input;
+    int linecounter = 0;
+    while(getline(file, line)){//берем по строчке
+        int pos = 0;
+        linecounter += 1;
+//        indentcreation = 0;
+        
+        while(line != ""){
+
+            string token = line.substr(0, line.find(delimeter));//берем новый токен
+            
+            if(token == "/*"){                                       //comments noindent influence
+                while(token != commentstopper){
+                    token = line.substr(0, line.find(delimeter));
+                    pos += token.length() + 1;
+                    line = line.substr(token.length());
+                    while(line[0] == ' ') line = line.substr(1);
+                }
             }
+            
+            else if(reserved[token] != ""){                          //reserved
+                cout << reserved[token] << ", -" << endl;
+                if(indents_allowers[token] != "") indentcreation = 1; //отслеживание предпологаемого обозначения новой переменной
+            }
+            
+            else if(symbols[token] != ""){                           //symbols
+                cout << symbols[token] << ", -" << endl;
+                if (symbols[token] == "T_open_brace") functionbody = 1; //отслеживание нахождения в теле функции
+                if (symbols[token] == "T_close_brace") functionbody = 0;
+            }
+            
+            else if(token[0] == '\"'){
+                cout << "T_stringcon, " << token;
+                cout << endl;
+            }
+            
+            else if(token != ""){
+                if(find(begin(indentslist), end(indentslist), token) != end(indentslist) /* and functionbody == 1*/){  //если такая переменная уже cуществует вывести
+                    cout << "T_indent, " << token << endl;
+                    
+                }else if(indentcreation == 1){                                              //если переменную можно создать + создание
+                    indentslist[numberofindents] = token;
+                    numberofindents += 1;
+                    cout << "T_indent, " << token << endl;
+                    //indentcreation = 0; //завершение создание переменной
+                    
+                }
+                else if(isNumber(token)){                                                                 //если это число
+                    cout << "T_indent, " << token << endl;
+                    
+                }else{
+                    cout << endl << "Error on " << linecounter << ":" << pos + 1 << endl;
+                    cout << "Problematic token is: " << token << endl;
+                    return true;
+                }
+                
+            }
+            
+            if(indentcreation == 1 and indentcreation_expire == 1){indentcreation = 0; indentcreation_expire = 0;}
+            else if(indentcreation == 1 and indentcreation_expire == 0){indentcreation_expire = 1;}
+            pos += token.length() + 1;
+            if (line != "") line = line.substr(token.length());
+            while(line[0] == ' ') line = line.substr(1);
         }
-        
-        else if(reserved[input] != ""){                          //reserved
-            cout << reserved[input] << ", -" << endl;
-        }
-        
-        else if(symbols[input] != ""){                           //symbols
-            cout << symbols[input] << ", -" << endl;
-        }
-        
-        else if(input[0] == '\"'){
-            cout << "T_stringcon, " << input;
-            cout << endl;
-        }
-        
-        else cout << "T_indent, " << input << endl;             //indents
     }
-
-
+    if(functionbody == 1){ cout << " SECOND } NOT FOUND" << endl; return false;}
     return true;
+
 }
 
 int main() {
     int taskn = -1;
     cout << "Task number (2/3): ";
-    cin >> taskn;
+//    cin >> taskn;
+    taskn = 3;
     if (taskn == 2) task2();
     if (taskn == 3) lexer();
     return 0;
